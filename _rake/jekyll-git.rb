a1ab40faa9b1a012a34d3aefee5ab777272e3135
Joris Pragt <pragtich@github>
2013-03-26 16:07:22 +0100

    Made config variables local

--- _rake/jekyll-git.rb
+++ _rake/jekyll-git.rb
@@ -10,32 +10,33 @@ module JekyllGit
         error 'No site configuration found' unless File.file?('_config.yml')
 
         # Get configuration
+
+        # config to be gotten from the config file later; 
+        @src_branch = 'source'
+        @dst_branch = 'master'
+        @dst_remote = 'origin'
+
+        @site_folder = '_site/'
       end
 
       def run(params={})
-        # config to be gotten somewhere else later; Move to initialize?
-        src_branch = 'source'
-        dst_branch = 'master'
-        dst_remote = 'origin'
 
-        site_folder = '_site/'
+        error 'No source branch found in deployment configuration' if @src_branch.nil?
+        error 'No destination branch found in deployment configuration' if @dst_branch.nil?
+        error 'No destination remote found in deployment configuration' if @dst_remote.nil?
 
-        error 'No source branch found in deployment configuration' if src_branch.nil?
-        error 'No destination branch found in deployment configuration' if dst_branch.nil?
-        error 'No destination remote found in deployment configuration' if dst_remote.nil?
-
-        error 'No site folder location found in deployment configuration' if site_folder.nil?
+        error 'No site folder location found in deployment configuration' if @site_folder.nil?
         
         git = ::Git::Base.open('.')
         
         # Check out the source branch
         # Ideally, I would not do this, but rather have a prerequesite to :compile that checks weither I am on source and am clean
-        puts "Checking out #{src_branch}."
+        puts "Checking out #{@src_branch}."
         git.checkout(src_branch)
 
         # Check out the destination branch
         puts "Checking out destination branch."
-        git.checkout(dst_branch)
+        git.checkout(@dst_branch)
 
 =begin No need to manually clean up: could use git clean -e _site/ instead
         # Cleanup and Copy output files recursively into the current directory
@@ -57,22 +58,22 @@ module JekyllGit
 =end
         # Copy files to proper place
         puts "Copying files."
-        FileUtils.cp_r(site_folder.chomp('/') + '/.', '.')
+        FileUtils.cp_r(@site_folder.chomp('/') + '/.', '.')
         
         # Remove _site folder
-        puts "Nuking site folder (#{site_folder})"
-        FileUtils.rm_rf(site_folder, :secure => true)
+        puts "Nuking site folder (#{@site_folder})"
+        FileUtils.rm_rf(@site_folder, :secure => true)
         
         # commit and push
         puts "Committing files to repo."
         time = Time.now
         git.commit_all("Committed by Rake at #{time.inspect}.")
         puts "Pushing to remote (#{dst_remote})."
-        git.push(git.remote(dst_remote))
+        git.push(git.remote(@dst_remote))
 
         # Checkout source
-        puts "Checking out #{src_branch}."
-        git.checkout(src_branch)
+        puts "Checking out #{@src_branch}."
+        git.checkout(@src_branch)
 
       end
       
